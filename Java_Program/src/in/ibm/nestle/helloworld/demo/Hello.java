a0d954d26a0952bfc02ac012c5049ecbff4775c7
Lenovo <Lenovo@KRISHNA>
2024-05-28 09:48:29 +0530

    first commit

---
+++ Java_Program/src/in/ibm/nestle/helloworld/demo/Hello.java
@@ -0,0 +1,10 @@
+package in.ibm.nestle.helloworld.demo;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		}
+
+}
